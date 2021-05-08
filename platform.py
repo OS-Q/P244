127792911bc93669b622bf5705a785bd469f6018
qitas <qitas@qitas.cn>
2021-05-08 14:57:42 +0800

    fix

--- platform.py
+++ platform.py
@@ -7,7 +7,7 @@ from platform import system
 from platformio.managers.platform import PlatformBase
 from platformio.util import get_systype
 
-class P25APlatform(PlatformBase):
+class P25Platform(PlatformBase):
 
     def configure_default_packages(self, variables, targets):
         board = variables.get("board")
